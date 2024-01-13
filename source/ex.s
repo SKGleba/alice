916acbc2ef5984c2f236866cd5c4f1a35ae52e30
Sebastian Kubala <30833773+SKGleba@users.noreply.github.com>
2024-01-13 14:52:12 +0100

    reset env cleanup

--- source/ex.s
+++ source/ex.s
@@ -34,14 +34,63 @@ PREHANDLE FIQ
 exc_RESET:
 	# disable irq, fiq, abt | enter system mode
 	cpsid	aif, #0x1F
+	isb SY
 
 	# clear core local record and global of a *ex caller
 	clrex
+	dsb SY
+
+	# disable all features
+	mov r0, #0x0
+	mcr p15, 0, r0, c1, c0, 0
+	dsb SY
+	isb SY
+
+	# invalidate bp entries
+	mcr p15, 0, r0, c7, c1, 6
+	mcr p15, 0, r0, c7, c5, 6
+	dsb SY
+	isb SY
+
+	# invalidate instruction cache, flush bp
+	mcr p15, 0, r0, c7, c1, 0
+	mcr p15, 0, r0, c7, c5, 0
+	dsb SY
+	isb SY
+
+	# invalidate cached TLB entries
+	mcr p15, 0, r0, c8, c3, 0
+	mcr p15, 0, r0, c8, c7, 0
+	dsb SY
+
+	# warm reset TTBCR
+	mcr p15, 0, r0, c2, c0, 2
+	dsb SY
 
 	# get current core
 	mrc	p15, 0, r1, c0, c0, 5
 	and	r1, r1, #0xF
 
+	# clean & invalidate the shared dcache, cant use sw mutexes as they are data
+	# so we assign a static region to each core
+	lsl r0, r1, #13
+	add r2, r0, #0x2000
+1:
+	mcr p15, 0, r0, c7, c14, 2
+	add r0, r0, #0x20
+	cmp r0, r2
+	bne 1b
+	dsb SY
+
+	# disable l2 cache ctrl
+	# TODO: remove?
+	movs r0, r1
+	bne 2f
+	movt r2, #0x1a00
+	str r0, [r2, #0x100]
+	dsb SY
+
+2:
 	# set sp per core based on config from vectors
 	ldr r0, =xcfg_sp_addr_per_core
 	add r0, r0, r1, lsl #2
@@ -52,9 +101,9 @@ exc_RESET:
 	blx	c_RESET
 
 	# ded
-1:
+3:
 	wfi
-	b 1b
+	b 3b
 
 
 # temporary handler for all non-reset exceptions, acts as soft reset
